f6c67e81a01f58a217dcce2b227a5fb003f29da5
Viktor Chuchurski <viktor@ssrd.io>
2018-05-27 16:38:03 +0200

    - widget list

--- app/src/main/java/io/viktorot/udacity_baking/widget/RecipeWidgetRemoteViewService.java
+++ app/src/main/java/io/viktorot/udacity_baking/widget/RecipeWidgetRemoteViewService.java
@@ -4,66 +4,71 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import io.viktorot.udacity_baking.Prefs;
 import io.viktorot.udacity_baking.R;
+import io.viktorot.udacity_baking.data.Ingredient;
+import io.viktorot.udacity_baking.data.Recipe;
 
 public class RecipeWidgetRemoteViewService extends RemoteViewsService {
 
-    private final RemoteViewsFactory FACTORY = new RemoteViewsFactory() {
-
-        private String title;
-
-        @Override
-        public void onCreate() {
-
-        }
-
-        @Override
-        public void onDataSetChanged() {
-            title = "123";
-        }
-
-        @Override
-        public void onDestroy() {
-
-        }
-
-        @Override
-        public int getCount() {
-            return 1;
-        }
-
-        @Override
-        public RemoteViews getViewAt(int i) {
-            RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_item_recipe_ingredient);
-            views.setTextViewText(R.id.name, "name");
-            views.setTextViewText(R.id.quantity, "123");
-
-            return null;
-        }
-
-        @Override
-        public RemoteViews getLoadingView() {
-            return null;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 1;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-    };
-
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return FACTORY;
+        return new RemoteViewsFactory() {
+
+            private Recipe recipe;
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                recipe = Prefs.getFavRecipe(getApplicationContext());
+            }
+
+            @Override
+            public void onDestroy() {
+
+            }
+
+            @Override
+            public int getCount() {
+                if (recipe == null) {
+                    return 0;
+                }
+                return recipe.ingredients.size();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int i) {
+                RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_item_recipe_ingredient);
+                Ingredient ing = recipe.ingredients.get(i);
+                views.setTextViewText(R.id.name, ing.name);
+                views.setTextViewText(R.id.quantity, String.format("%f %s", ing.quantity, ing.measure));
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return null;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return i;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+        };
     }
 }
